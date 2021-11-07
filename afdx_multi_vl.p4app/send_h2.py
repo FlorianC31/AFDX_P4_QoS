c1a37b07c635ea3bd514b8a2eb0bee9d82e8d991
adilmrk <33380964+adilmrk@users.noreply.github.com>
2021-11-07 09:34:29 +0100

    Add files via upload

---
+++ afdx_multi_vl.p4app/send_h2.py
@@ -0,0 +1,20 @@
+from scapy.all import Ether, Raw, sendp
+import sys
+import random, string
+import time
+
+def randomword(max_length):
+    length = random.randint(1, max_length)
+    return ''.join(random.choice(string.lowercase) for i in range(length))
+
+VL_dst = "03:00:00:00:00:01"
+Src_const = "02:00:00"
+data = "12:34:56:78:90" # data = randomword(2)
+
+p = Ether(dst=VL_dst,src=Src_const)/Raw(load=data)
+print ' ----------------- I M ALIVE'
+print p.show()
+
+while True:
+    sendp(p, iface = 'h2-eth0')
+    time.sleep(1/10)
