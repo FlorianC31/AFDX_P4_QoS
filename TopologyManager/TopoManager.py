7af37db08374346cd6636015c827f522b1804351
FlorianC31 <f.champain@gmail.com>
2021-11-08 11:47:34 +0100

    Comments and minor corrections

--- TopologyManager/TopoManager.py
+++ TopologyManager/TopoManager.py
@@ -1,23 +1,50 @@
-TOPO_FILE = "input_topo.txt"
+import os
+import sys
+
 PACKET_SIZE = 64
 TABLE_NAME = "afdx_table"
 ACTION_NAME = "Check_VL"
 MC_DUMP = True
 
+_help = """
+Automatic creation p4app files with given topology file
+Launching command: 
+python3 TopoManager.py <input_topo_file> [<destination path>]
+Destination path is current directory by default
+
+Input : 
+- Input topology file (see 'input_topo.txt' as example)
+
+Outputs:
+- topo.txt file (to be added in the p4app package)
+- commands_x.txt file : switch table for each switch (to be added in the p4app package)
+- check_VLx.sh files to be executed after running p4app to check the VLs
+"""
+
 
 class VirtualLink:
+
     def __init__(self, data_line):
+        """ Class containing the Virtual links Data """
+
         self.id = data_line[0]
         self.paths = []
         self.add_path(data_line)
 
     def add_path(self, data_line):
-        if type(data_line[-1]) != str:
+        """ Add the VL path details from the data line form topo input file.
+         In case of multicast VL, there will be a path by destination host """
+
+        if not data_line[-1].isdecimal():
+            # if the max packet size is not defined at the end of the line, all the fields are considered as entities
             self.paths.append(data_line[1:])
         else:
+            # Else, the last field (max packet size) is ignored
             self.paths.append(data_line[1:-1])
 
     def print_paths(self):
+        """ Print the VL details """
+
         print('')
         print('VL_ID:', self.id)
         for path in self.paths:
@@ -26,27 +53,47 @@ class VirtualLink:
 
 class Switch:
     def __init__(self, switch_id, distant_entity):
+        """ Class containing the switches Data
+            Each entity connected to the switch is attributed to a port number from 1.
+            Therefore, all the the entity are stored in the 'ports' list :
+            - the ports[0] is '-'
+            - then, ports[port_id] = connected entity name (switch or host)
+
+            The connection dict contains the connexion between ingress port and outgress ports list for each VL
+        """
+
         self.id = switch_id
-        self.ports = ['', distant_entity]
-        self.connections = {}
+        self.ports = ['', distant_entity]  # Creation of the port list with the first entity
+        self.connections = {}  #
 
     def add_port(self, distant_entity):
+        """ Addition of an entity in the port list """
+
         self.ports.append(distant_entity)
 
     def add_connection(self, vl):
+        """ For a given VL and the current switch: Addition of connexions between ingress port and outgress ports list
+        The outgress port is a list because it can contain several ports in case of multicast VL.
+        """
+
         for path in vl.paths:
             if self.id in path:
-                ingress_entity = path[path.index(self.id) - 1]  # get the previous entity in the path
-                outgress_entity = path[path.index(self.id) + 1]  # get the next entity in the path
+                ingress_entity = path[path.index(self.id) - 1]  # get the ingress entity in the path
+                outgress_entity = path[path.index(self.id) + 1]  # get the outgress entity in the path
                 ingress_port = self.ports.index(ingress_entity)
                 outgress_port = self.ports.index(outgress_entity)
 
-                if vl.id not in self.connections:  # if the VL is not existing on the switch
+                if vl.id not in self.connections:  # if the VL is not existing yet on the switch
                     self.connections[vl.id] = {'ingress_port': ingress_port, 'outgress_port': [outgress_port]}
                 else:  # if it already exists (multicast VL), the outgress port is added in the outgress ports list
                     self.connections[vl.id]['outgress_port'].append(outgress_port)
 
     def print(self):
+        """ Print the switches details:
+        - Detail of entity (switch or host) connected of each switch port
+        - Detail of the ingress / outgress ports connexion for each VL
+        """
+
         print('')
         print('Switch:', self.id)
         i = 0
@@ -61,8 +108,9 @@ class Switch:
 
 class Topology:
     def __init__(self, topo_filename):
+        """ Class containing all the switches, host, VL and physical connections of the input topology """
 
-        self.connections = []
+        self.connections = []  # Physical connections
         self.vls = {}
         self.switches = {}
         self.hosts = []
@@ -71,34 +119,41 @@ class Topology:
         self.create_switch_connections()
 
     def read_input_file(self, topo_filename):
+        """ Reading and processing input topo file"""
 
         with open(topo_filename, 'r') as topo_file:
             line = topo_file.readline()
 
             # Reading of links
-            while line[:7] != "--start" and line != '':
+            while not line.startswith("--start") and line != '':
                 line = topo_file.readline()
             line = topo_file.readline()  # Jump the line "--start"
 
-            while line[:6] != "--stop" and line != '':
-                self.read_link(line)
+            while not line.startswith("--stop") and line != '':
+                self.read_link(line.strip())
                 line = topo_file.readline()
 
             # Reading of virtual links
-            while line[:7] != "--start" and line != '':
+            while not line.startswith("--start") and line != '':
                 line = topo_file.readline()
             line = topo_file.readline()  # Jump the line "--start"
 
-            while line[:6] != "--stop" and line != '':
-                self.read_vls(line)
+            while not line.startswith("--stop") and line != '':
+                self.read_vls(line.strip())
                 line = topo_file.readline()
 
-    def create_switch_connections(self):
-        for switch in self.switches:
-            for vl in self.vls:
-                self.switches[switch].add_connection(self.vls[vl])
+    def read_link(self, data_line):
+        """ Reading links in topo input file """
+
+        data = data_line.split(',')
+        self.connections.append(data[0:2])
+
+        self.read_entity(data, 0)
+        self.read_entity(data, 1)
 
     def read_entity(self, switch_data, i):
+        """ For each entity of the link line, store it in switch dict or host list """
+
         entity_id = switch_data[i]
         distant_entity = switch_data[1-i]
 
@@ -115,24 +170,28 @@ class Topology:
         else:
             raise NameError("The entity in the links list has to start with 's' for swhitchs or 'h' for hosts")
 
-    def read_link(self, data_line):
-        data = data_line.split(',')
-        self.connections.append(data[0:2])
-
-        self.read_entity(data, 0)
-        self.read_entity(data, 1)
-
     def read_vls(self, data_line):
+        """ Read vls line from input topo file and store them in vls dict """
+
         data = data_line.split(',')
 
         # if the virtual link is not existing yet, it's created
         if data[0] not in self.vls:
             self.vls[data[0]] = VirtualLink(data)
 
-        else:  # else, the VL is completed with the new path
+        else:  # else, the VL is completed with the new path (in case of multicast VL)
             self.vls[data[0]].add_path(data)
 
+    def create_switch_connections(self):
+        """ For each switch : a in/out port connection is created for each VL passing by this switch """
+
+        for switch in self.switches:
+            for vl in self.vls:
+                self.switches[switch].add_connection(self.vls[vl])
+
     def write_topo_file(self, filename):
+        """ Creation of the topo file to be given in input in the p4app package """
+
         with open(filename, 'w') as file:
             file.write("switches " + str(len(self.switches)) + '\n')
             file.write("hosts " + str(len(self.hosts)) + '\n')
@@ -141,16 +200,18 @@ class Topology:
                 file.write(str(' '.join(connect)) + '\n')
 
     def write_switch_cmd_files(self):
+        """ Creation of the command.txt file for each switch for the p4app input package """
+
         for switch in self.switches:
             with open("commands_" + switch[1:] + ".txt", 'w') as file:
                 node_handle = 0
-                groupe_id = 1
+                group_id = 1
                 for connect in self.switches[switch].connections:
                     vl_id = str(connect[2:])
                     ingress_port = str(self.switches[switch].connections[connect]['ingress_port'])
                     outgress_ports = " ".join(map(str, self.switches[switch].connections[connect]['outgress_port']))
 
-                    mc_mgrp_create_line = ("mc_mgrp_create", str(groupe_id), '\n')
+                    mc_mgrp_create_line = ("mc_mgrp_create", str(group_id), '\n')
                     table_add_line = ("table_add",
                                       TABLE_NAME,
                                       ACTION_NAME,
@@ -158,10 +219,10 @@ class Topology:
                                       vl_id,
                                       "=>",
                                       str(PACKET_SIZE),
-                                      str(groupe_id),
+                                      str(group_id),
                                       '\n')
                     mc_node_create_line = ("mc_node_create", str(node_handle), outgress_ports, '\n')
-                    mc_node_associate_line = ("mc_node_associate", str(groupe_id), str(node_handle), '\n')
+                    mc_node_associate_line = ("mc_node_associate", str(group_id), str(node_handle), '\n')
 
                     file.write(" ".join(mc_mgrp_create_line))
                     file.write(" ".join(table_add_line))
@@ -170,15 +231,20 @@ class Topology:
                     file.write('\n')
 
                     node_handle += 1
-                    groupe_id += 1
+                    group_id += 1
 
                 if MC_DUMP:
                     file.write("mc_dump\n")
 
     def write_check_files(self):
+        """ Writing of checking script for each VL. These scripts perform 2 actions :
+        - Calling sniffing script with all the host and switch ports crossed by the current VL
+        - Sending a packet on the source host of the current VL """
+
         for vl in self.vls:
             with open("check_" + vl + ".sh", 'w') as file:
-                ligne = ["python3", "sniffer.py"]
+                # Calli sniffing script with all the host and switch ports crossed by the current VL
+                output_line = ["python3", "sniffer.py"]
                 for path in self.vls[vl].paths:
                     for entity in path:
                         if entity[0] == 's':  # if the entity is a switch
@@ -186,26 +252,34 @@ class Topology:
                             previous_entity = path[path.index(entity) - 1]
                             ingress_port = self.switches[entity].ports.index(previous_entity)
                             ingress = entity + "-eth" + str(ingress_port)
-                            if ingress not in ligne:
-                                ligne.append(ingress)
+                            if ingress not in output_line:
+                                output_line.append(ingress)
 
                             # Outgress interface
                             next_entity = path[path.index(entity) + 1]
                             outgress_port = self.switches[entity].ports.index(next_entity)
                             outgress = entity + "-eth" + str(outgress_port)
-                            if outgress not in ligne:
-                                ligne.append(outgress)
+                            if outgress not in output_line:
+                                output_line.append(outgress)
 
                         else:  # if it's a host
-                            if entity not in ligne:
-                                ligne.append(entity)
-                file.write(" ".join(ligne[:10]))
+                            if entity not in output_line:
+                                output_line.append(entity)
+                file.write(" ".join(output_line[:10]))
                 file.write(" &\n")
                 host = self.vls[vl].paths[0][0]
+
+                # Send a packet on the source host of the current VL
                 file.write("p4app exec m " + host + " python ../tmp/send_afdx_packet.py " + vl + ' ' + host)
 
     def print(self):
-        print(self.connections)
+        """ Print the topology details:
+        - Physical connections
+        - VLs details
+        - Switches details
+        """
+
+        print("Physical connections :", self.connections)
         for vl in self.vls:
             self.vls[vl].print_paths()
 
@@ -214,8 +288,17 @@ class Topology:
 
 
 if __name__ == "__main__":
-    new_topo = Topology(TOPO_FILE)
-    new_topo.print()
-    new_topo.write_topo_file("topo.txt")
-    new_topo.write_check_files()
-    new_topo.write_switch_cmd_files()
+    try:
+        script_name, topo_input_file, *_ = sys.argv
+        dest_path = sys.argv[-1] if sys.argv[-1] != topo_input_file else '.'
+
+        new_topo = Topology(topo_input_file)  # Creation of the topology structure
+        new_topo.print()  # Print the topology in console
+
+        os.chdir(dest_path)
+        new_topo.write_topo_file("topo.txt")  # Write the topo file for p4app
+        new_topo.write_check_files()  # Write all the check_vl.sh files to check the VL
+        new_topo.write_switch_cmd_files()  # Write de command.txt table for each switch (for p4app)
+
+    except ValueError:
+        print("ERROR: missing argument" + _help)
