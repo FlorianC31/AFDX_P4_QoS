a13eea5acab17c3abd58e1f2057889d9455b10bd
adilmrk <33380964+adilmrk@users.noreply.github.com>
2022-01-22 20:41:52 +0100

    Adding line histogram drawing for VLs delays
    (as seen in afdx lecture)

--- analysis/log_analyser.py
+++ analysis/log_analyser.py
@@ -3,15 +3,19 @@
 ##
 
 from datetime import datetime
+import numpy
+import matplotlib.pyplot as plt
+import scipy.stats
 
-
-ingress_logs = ["parsed_s1-eth1","parsed_s1-eth2"]
-engress_log = "parsed_s1-eth3"
+ingress_logs = ["parsed_s1-eth1","parsed_s1-eth2","parsed_s1-eth3"]
+engress_log = "parsed_s1-eth4"
 vls_to_analyse = ["03:00:00:00:00:01","03:00:00:00:00:02","03:00:00:00:00:03"]
 format_string = "%H:%M:%S.%f" # time format needed for comparison
-
-vls_average_switch_pass_time = []
+n_bins = 10 # number of histrogram bins
+histrogram_data = [[] for vl in vls_to_analyse]
+indx = -1
 for vl in vls_to_analyse: # loop through all vls
+    indx = indx + 1
     total_passing_through_switch_time = 0
     number_of_packets = 0
     for ingress_log in ingress_logs: # loop through ingress_log
@@ -31,7 +35,21 @@ for vl in vls_to_analyse: # loop through all vls
                         diff = departure_time - entry_time
                         # Get the interval in microseconds
                         diff_in_micro_secs = diff.total_seconds() * 1000000
+                        # Calculations needed for average
                         total_passing_through_switch_time = total_passing_through_switch_time + diff_in_micro_secs
                         number_of_packets = number_of_packets + 1
+                        # Calculations needed for histrogram plotting
+                        histrogram_data[indx].append(diff_in_micro_secs)
     average_time = int(total_passing_through_switch_time / number_of_packets)
-    print("Vl : "+ str(vl) + " || average time : "+str(average_time))
+    print("Vl : "+ str(vl) + " || average switching time : "+str(average_time)+" ms")
+
+# histogram plotting
+for i in range(len(vls_to_analyse)):
+    n, bins = numpy.histogram(numpy.array(histrogram_data[i]), n_bins)# plt.hist(x=numpy.array(histrogram_data[i]), bins=n_bins, color='#0504aa',alpha=0.7, rwidth=0.85)
+    vals = n/sum(n)
+    plt.plot(bins[:-1], vals)
+plt.legend(vls_to_analyse)
+plt.xlabel("switching delay")
+plt.ylabel("% of VL packets")
+plt.title("Switching delays comparison")
+plt.show()
