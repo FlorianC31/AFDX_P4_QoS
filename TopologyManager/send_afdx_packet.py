e28ab87d30d5e713580299f72a42ab118255ba40
FlorianC31 <72257044+FlorianC31@users.noreply.github.com>
2022-01-29 14:03:37 +0100

    add BAG management

--- TopologyManager/send_afdx_packet.py
+++ TopologyManager/send_afdx_packet.py
@@ -1,22 +1,27 @@
+# Script to send VL packet from p4app host
+# This script should be direclty launched with the check_VL.sh scripts
+
 from scapy.all import Ether, Raw, sendp
 import sys
 import random, string
 import time
 
-name, vl, host = sys.argv
+# Get arguments
+name, vl, host, bag = sys.argv
 
-def randomword(max_length):
-    length = random.randint(1, max_length)
-    return ''.join(random.choice(string.lowercase) for i in range(length))
-
+# packet data
 VL_dst = "03:00:00:00:00:0" + vl[-1]
 Src_const = "02:00:00"
 data = "12:34:56:78:90" # data = randomword(2)
 
+# construction of the packet
 p = Ether(dst=VL_dst,src=Src_const)/Raw(load=data)
-print ' ----------------- I M ALIVE'
-print p.show()
+print(p.show())
 
+# loop to send packets taking into account the BAG
+send_time = time.time()
 while True:
-    sendp(p, iface = host + '-eth0')
-    time.sleep(1/10)
+    time_t = time.time()
+    if time_t >= send_time + bag / 1000:
+        send_time = time_t
+        sendp(p, iface = host + '-eth0')
