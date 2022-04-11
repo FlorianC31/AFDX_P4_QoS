d56277e96a64d7b1694fd133416fbfc33de54659
FlorianC31 <72257044+FlorianC31@users.noreply.github.com>
2022-04-11 13:43:39 +0200

    p4pi management added

--- TopologyManager/TopoManager.py
+++ TopologyManager/TopoManager.py
@@ -28,12 +28,12 @@ DEFAULT_PRIO = 1
 PI_ACTION_FCT = "Check_VL"
 
 _help = """
-Automatic creation p4app files with given topology file
+Automatic creation switch command and VL check files with given topology file.
 Launching command: 
 python3 TopoManager.py <input_topo_file> [<destination path>]
 Destination path is current directory by default
 Input : 
-- Input topology file (see 'input_topo.txt' as example)+
+- Input topology file (see 'input_topo.txt' as example)
 - Output platform : 1 for p4app on mininet, 2 for p4pi on Raspberry
 - [optional] destination path for the output files (default value, same repertory as the script) 
 Outputs:
